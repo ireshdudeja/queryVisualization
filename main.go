2a6a1e709a1e4c70ce25f4978e4ab8c93202ba44
Iresh Dudeja <dudeja.iresh@gmail.com>
2018-12-11 23:22:18 +0100

    Implemented using REST API
    - Added json file and reading data from json file
    - Sending json data using webservice so that it can be read by index.html file
    - Can also read json from the postman (using mock server)

--- main.go
+++ main.go
@@ -1,26 +1,76 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
 )
 
 type City struct {
-	Names      []string
-	Population []int64
+	Name       string `json:"name"`
+	Population string `json:"population"`
+}
+
+type Cities struct {
+	Cities []City `json:"cities"`
 }
 
 func main() {
 	http.HandleFunc("/", ShowCities)
+	http.HandleFunc("/api", GetCitiesJson)
+	readJsonFile()
 	http.ListenAndServe(":4040", nil)
+
+}
+
+var cities Cities
+
+func readJsonFile() {
+	// Open our jsonFile
+	jsonFile, err := os.Open("cities.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened jsonFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	/*
+		var result map[string]interface{}
+		json.Unmarshal([]byte(byteValue), &result)
+
+		fmt.Println(result["cities"])
+	*/
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &cities)
+
+	/*
+		for i := 0; i < len(cities.Cities); i++ {
+			fmt.Println("City Name: " + cities.Cities[i].Name)
+			fmt.Println("City Population: " + cities.Cities[i].Population)
+		}
+	*/
+
+}
+
+func GetCitiesJson(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(cities)
+
 }
 
 func ShowCities(w http.ResponseWriter, r *http.Request) {
-	book := City{
-		[]string{"Erfurt", "Jena", "Gera", "Weimar", "Ilmenau"},
-		[]int64{204994, 105129, 99262, 65479, 25984},
-	}
 
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
@@ -29,7 +79,7 @@ func ShowCities(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := tmpl.Execute(w, book); err != nil {
+	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
