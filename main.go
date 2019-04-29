e10d72572980bd62cac5d0093c86305c8a024481
Iresh Dudeja <dudeja.iresh@gmail.com>
2019-04-30 00:52:43 +0200

    [Go, JSON] Read Query Parameters from URL
    - changed JSON format, added parameters key, removed group key

--- main.go
+++ main.go
@@ -19,24 +19,24 @@ type City struct {
 	StudentsCount string `json:"count"`
 }
 
-type OperatorData struct {
-	Attributes map[string]string `json:"attributes"`
-	Return     map[string]string `json:"return"`
-}
+// type OperatorData struct {
+// 	Attributes map[string]string `json:"attributes"`
+// 	Return     map[string]string `json:"return"`
+// }
 
 type Node struct {
-	ID     string       `json:"id"`
-	Label  string       `json:"label"`
-	Group  int          `json:"group"`
-	Level  int          `json:"level"`
-	Cities []City       `json:"data"`
-	Info   OperatorData `json:"operatorData"`
+	ID         string                 `json:"id"`
+	Label      string                 `json:"label"`
+	Level      int                    `json:"level"`
+	Cities     []City                 `json:"data"`
+	Parameters map[string]interface{} `json:"parameters"`
+	//Info   OperatorData `json:"operatorData"`
 }
 
 type Link struct {
-	Target   string  `json:"target"`
-	Source   string  `json:"source"`
-	Strength float64 `json:"strength"`
+	Target string `json:"target"`
+	Source string `json:"source"`
+	//Strength float64 `json:"strength"`
 }
 type Query struct {
 	Nodes []Node `json:"nodes"`
@@ -52,15 +52,8 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "4040"
-		//return "", fmt.Errorf("$PORT not set")
-	}
-	log.Println("Port Number: " + port)
-	return ":" + port, nil
-}
+var router = mux.NewRouter()
+
 func main() {
 
 	addr, err := determineListenAddress()
@@ -68,10 +61,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", showQuery)
-	router.HandleFunc("/api", getQueryJSON)
-	router.HandleFunc("/api/json", postJSONData)
+	router.HandleFunc("/", showQueryHandler).Methods("GET")
+	router.HandleFunc("/api", displayQueryJSONHandler)
+	router.HandleFunc("/api/json", processReceivedQueryJSONHandler)
+	//router.HandleFunc("/modify", OperatorParametersHandler).Methods("GET")
 	router.HandleFunc("/echo", wsHandler)
 	go echo()
 
@@ -83,22 +76,26 @@ func main() {
 	}
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4040"
+		//return "", fmt.Errorf("$PORT not set")
 	}
-	log.Printf("ws handler is called!")
-
-	// register client
-	clients[ws] = true
+	log.Println("Server Listening on Port Number: " + port)
+	return ":" + port, nil
 }
 
 var query Query
 
-func showQuery(w http.ResponseWriter, r *http.Request) {
+func showQueryHandler(w http.ResponseWriter, r *http.Request) {
 
-	readJSONFile()
+	// vars := mux.Vars(r)
+	// fmt.Printf("Route Variable: %s\n", vars["queryid"])
+	// Add a switch condition to read different
+
+	readJSONFile(w)
+
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
 
@@ -110,15 +107,64 @@ func showQuery(w http.ResponseWriter, r *http.Request) {
 	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	v := r.URL.Query()
+
+	// fmt.Printf("Query is: %v", v)
+
+	operatorId := v.Get("id")
+	// email := v.Get("email")
+	// fmt.Printf("Operator ID: %s, Email: %s", operatorId, email)
+
+	/*
+		Removing key value pair for "id"
+		otherwise allParametersExists will be false for
+		_, ok := node.Parameters[key] in code below
+	*/
+	delete(v, "id")
+
+	for _, node := range query.Nodes {
+
+		fmt.Printf("Node name: %s\n", node.Label)
+		if node.ID == operatorId {
+			allParametersExists := true
+
+			fmt.Printf("  ID exists\n")
+			for key, value := range v {
+
+				if _, ok := node.Parameters[key]; ok {
+					fmt.Printf("	KEY EXISTS and NEW VALUE. %s = %s\n", key, value)
+
+				} else {
+					allParametersExists = false
+					fmt.Println("allParametersExists value ", allParametersExists, value)
+				}
+
+			}
+
+			if allParametersExists {
+				for key, value := range v {
+					node.Parameters[key] = value[0]
+				}
+				fmt.Printf("Updated Paramters Dict %v", node.Parameters)
+
+			}
+
+		}
+	}
+
 }
 
-func readJSONFile() {
+// Reading data from json file and populate struct variable
+func readJSONFile(w http.ResponseWriter) {
 
 	// Open our jsonFile
 	jsonFile, err := os.Open("output.json")
 
 	// if we os.Open returns an error then handle it
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
 		fmt.Println(err)
 	}
 
@@ -131,13 +177,23 @@ func readJSONFile() {
 	var result map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &result)
 
-	fmt.Println(result["nodes"])
+	fmt.Printf("Read JSON data: \n%+v\n", result["nodes"])
 
 	// unmarshal byteArray
 	json.Unmarshal(byteValue, &query)
+
+	fmt.Printf("\n Only Paramters: %v\n", query.Nodes[0].Parameters)
 }
 
-func getQueryJSON(w http.ResponseWriter, r *http.Request) {
+// func OperatorParametersHandler(w http.ResponseWriter, r *http.Request) {
+// 	v := r.URL.Query()
+
+// 	username := v.Get("username")
+// 	email := v.Get("email")
+// 	fmt.Printf("User Name: %s, Email: %s", username, email)
+// }
+
+func displayQueryJSONHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -145,7 +201,7 @@ func getQueryJSON(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func postJSONData(rw http.ResponseWriter, req *http.Request) {
+func processReceivedQueryJSONHandler(rw http.ResponseWriter, req *http.Request) {
 
 	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&query)
@@ -173,11 +229,22 @@ func writer(query *Query) {
 	broadcast <- query
 }
 
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ws handler is called!")
+
+	// register client
+	clients[ws] = true
+}
+
 func echo() {
 	for {
 		val := <-broadcast
-		query := fmt.Sprintf("%s", val)
-		log.Println(query)
+		query := fmt.Sprintf("Value from Channel: %s", val)
+		//log.Println(query)
 		// send to every client that is currently connected
 		for client := range clients {
 			err := client.WriteMessage(websocket.TextMessage, []byte(query))
