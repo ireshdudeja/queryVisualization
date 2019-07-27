6488663d96e4438e326b71aad6cb1b0ab88a4ade
Iresh Dudeja <dudeja.iresh@gmail.com>
2019-07-28 00:42:10 +0200

    [js,html] Added Speedometer using Highcharts

--- static/js/script.js
+++ static/js/script.js
@@ -318,17 +318,130 @@
 
                 switch (visualization) {
                     case 'progressBar':
-                    let progressDiv = chartDiv.appendChild(document.createElement('div')); 
-                    progressDiv.setAttribute("class","progress");
-                    let progressBarDiv = progressDiv.appendChild(document.createElement('div')); 
-                    progressBarDiv.setAttribute("class","progress-bar");
-                    progressBarDiv.setAttribute("role","progressbar");
-                    progressBarDiv.setAttribute("style","width:"+initialValue+"%;");
-                    progressBarDiv.setAttribute("aria-valuenow",initialValue);
-                    progressBarDiv.setAttribute("aria-valuemin","0");
-                    progressBarDiv.setAttribute("aria-valuemax","100");
-                    progressBarDiv.innerText = initialValue + "%";
-                      break;
+                        let progressDiv = chartDiv.appendChild(document.createElement('div')); 
+                        progressDiv.setAttribute("class","progress");
+                        let progressBarDiv = progressDiv.appendChild(document.createElement('div')); 
+                        progressBarDiv.setAttribute("class","progress-bar");
+                        progressBarDiv.setAttribute("role","progressbar");
+                        progressBarDiv.setAttribute("style","width:"+initialValue+"%;");
+                        progressBarDiv.setAttribute("aria-valuenow",initialValue);
+                        progressBarDiv.setAttribute("aria-valuemin","0");
+                        progressBarDiv.setAttribute("aria-valuemax","100");
+                        progressBarDiv.innerText = initialValue + "%";
+                        break;
+
+                    case 'gauge':
+                        Highcharts.chart(key, {
+                            chart: {
+                                type: 'gauge',
+                                plotBackgroundColor: null,
+                                plotBackgroundImage: null,
+                                plotBorderWidth: 0,
+                                plotShadow: false
+                            },
+                        
+                            title: {
+                                text: ''
+                            },
+                        
+                            pane: {
+                                startAngle: -150,
+                                endAngle: 150,
+                                background: [{
+                                    backgroundColor: {
+                                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                                        stops: [
+                                            [0, '#FFF'],
+                                            [1, '#333']
+                                        ]
+                                    },
+                                    borderWidth: 0,
+                                    outerRadius: '109%'
+                                }, {
+                                    backgroundColor: {
+                                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                                        stops: [
+                                            [0, '#333'],
+                                            [1, '#FFF']
+                                        ]
+                                    },
+                                    borderWidth: 1,
+                                    outerRadius: '107%'
+                                }, {
+                                    // default background
+                                }, {
+                                    backgroundColor: '#DDD',
+                                    borderWidth: 0,
+                                    outerRadius: '105%',
+                                    innerRadius: '103%'
+                                }]
+                            },
+                        
+                            // the value axis
+                            yAxis: {
+                                min: 0,
+                                max: 8000, // getData() + 50 or get it from json 
+                        
+                                minorTickInterval: 'auto',
+                                minorTickWidth: 1,
+                                minorTickLength: 10,
+                                minorTickPosition: 'inside',
+                                minorTickColor: '#666',
+                        
+                                tickPixelInterval: 30,
+                                tickWidth: 2,
+                                tickPosition: 'inside',
+                                tickLength: 10,
+                                tickColor: '#666',
+                                labels: {
+                                    step: 2,
+                                    rotation: 'auto'
+                                },
+                                title: {
+                                    text: ''
+                                },
+                                plotBands: [{
+                                    from: 0,
+                                    to: 8000, // getData() + 50
+                                    color: '#80CAF6' // green
+                                }]
+                            },
+                        
+                            series: [{
+                                name: '', //Speed
+                                data: [getData()],
+                                tooltip: {
+                                    valueSuffix: '' // 'km/h'
+                                }
+                            }],
+
+                            exporting: { 
+                                enabled: false 
+                            }
+                        
+                        },
+
+                        
+                        // Add some life
+                        // function (chart) {
+                        //     if (!chart.renderer.forExport) {
+                        //         setInterval(function () {
+                        //             var point = chart.series[0].points[0],
+                        //                 newVal,
+                        //                 inc = Math.round((Math.random() - 0.5) * 20);
+                        
+                        //             newVal = point.y + inc;
+                        //             if (newVal < 0 || newVal > 200) {
+                        //                 newVal = point.y - inc;
+                        //             }
+                        
+                        //             point.update(newVal);
+                        
+                        //         }, 3000);
+                        //     }
+                        // }
+                        );
+                        break;
 
                     default:
                         var layout = {
@@ -644,7 +757,33 @@
                                         .css("width", getData() + "%")
                                         .attr("aria-valuenow", getData())
                                         .text(getData() + "%");
+                                break;
 
+                                case 'gauge':
+                                        var chart=$("#"+key).highcharts();
+                                        console.log("High Chart:", chart);
+
+                                        // function (chart) {
+                                            if (!chart.renderer.forExport) {
+                                                // setInterval(function () {
+                                                    var point = chart.series[0].points[0],
+                                                        newVal,
+                                                        inc = Math.round((Math.random() - 0.5) * 20);
+                                        
+                                                    // newVal = point.y + inc;
+                                                    // if (newVal < 0 || newVal > 200) {
+                                                    //     newVal = point.y - inc;
+                                                    // }
+
+                                                    newVal = getData();
+                                                    point.update(newVal);
+                                        
+                                                // }, 3000);
+                                            }
+                                        // }
+                                        
+
+                                break;
                                 default:
                                     // Update Chart
                                     function rand() {
